c96cdb6ca1ad6b71f2c988be2454c8ffa180e3eb
Richard Guy <rich.guy@cantab.net>
2018-11-30 21:15:10 +0000

    Fixed promises

--- app/static/app.js
+++ app/static/app.js
@@ -19,7 +19,6 @@ $(document).ready(function(){
 // Refresh data on page
 function refresh(){
 	document.getElementById("api-status").classList.add("fa-spinner")
-	console.log("Refreshing...");
 	Promise.all([
 		refreshOrders(),
 		refreshBalance(),
@@ -28,7 +27,6 @@ function refresh(){
 	])
 	.then(() => {
 		document.getElementById("api-status").classList.remove("fa-spinner");
-		console.log("...finished!");
 	});
 }
 
@@ -42,82 +40,92 @@ function cancelOrder(id){
 }
 
 // API call to get list of orders for account and write to table
-function refreshOrders(){
-	fetch("/api/orders/"+account)
-	.then(response => response.json())
-	.then(data => {
-		var orders = data;				
-		var table = document.getElementById("orderTableBody");
-		for(var i = table.rows.length - 1; i > -1; i--) {
-			table.deleteRow(i);
-		}
-		$.each(orders, function(){
-			var row = table.insertRow(-1);
-			var time = new Date(this.timestamp).toLocaleTimeString('en-GB', {
-				hour: '2-digit',
-				minute: '2-digit'
-			});
-			row.insertCell().innerHTML = time;
-			row.insertCell().innerHTML = this.stock;
-			row.insertCell().innerHTML = this.side;							
-			row.insertCell().innerHTML = this.volume;
-			row.insertCell().innerHTML = this.limit;
-			var btn = document.createElement("BUTTON");
-			btn.classList.add("btn", "btn-primary", "float-right");
-			btn.setAttribute("type", "button");
-			btn.id = this.id
-			var t = document.createTextNode("Cancel order");
-			btn.appendChild(t);
-			row.insertCell().appendChild(btn);
-			btn.onclick = function(){
-				cancelOrder(this.id);
-		}});
+function refreshOrders() {
+	return new Promise(function(resolve, reject) {
+		fetch("/api/orders/"+account)
+		.then(response => response.json())
+		.then(data => {
+			var orders = data;				
+			var table = document.getElementById("orderTableBody");
+			for(var i = table.rows.length - 1; i > -1; i--) {
+				table.deleteRow(i);
+			}
+			$.each(orders, function(){
+				var row = table.insertRow(-1);
+				var time = new Date(this.timestamp).toLocaleTimeString('en-GB', {
+					hour: '2-digit',
+					minute: '2-digit'
+				});
+				row.insertCell().innerHTML = time;
+				row.insertCell().innerHTML = this.stock;
+				row.insertCell().innerHTML = this.side;							
+				row.insertCell().innerHTML = this.volume;
+				row.insertCell().innerHTML = this.limit;
+				var btn = document.createElement("BUTTON");
+				btn.classList.add("btn", "btn-primary", "float-right");
+				btn.setAttribute("type", "button");
+				btn.id = this.id
+				var t = document.createTextNode("Cancel order");
+				btn.appendChild(t);
+				row.insertCell().appendChild(btn);
+				btn.onclick = function(){
+					cancelOrder(this.id);
+			}});
+			resolve()
+		})
 	})
 }
 
 // API call to get latest bid/ask prices for each stock and write to table
 function refreshPrices() {
-	fetch("/api/prices")
-	.then(response => response.json())
-	.then(data => {
-		var table = document.getElementById("priceTableBody");
-		for(var i = table.rows.length - 1; i > -1; i--) {
-			table.deleteRow(i);
-		}
-		$.each(data, function(){
-			var row = table.insertRow(-1);
-			row.insertCell().innerHTML = this.stock;
-			row.insertCell().innerHTML = this.best_bid;
-			row.insertCell().innerHTML = this.best_ask;
-			row.insertCell().innerHTML = this.last;						
-		});
+	return new Promise(function(resolve, reject) {
+		fetch("/api/prices")
+		.then(response => response.json())
+		.then(data => {
+			var table = document.getElementById("priceTableBody");
+			for(var i = table.rows.length - 1; i > -1; i--) {
+				table.deleteRow(i);
+			}
+			$.each(data, function(){
+				var row = table.insertRow(-1);
+				row.insertCell().innerHTML = this.stock;
+				row.insertCell().innerHTML = this.best_bid;
+				row.insertCell().innerHTML = this.best_ask;
+				row.insertCell().innerHTML = this.last;						
+			});
+			resolve()
+		})
 	})
 }
 
 // API call to get latest balance and write to page
-function refreshBalance(){
-	fetch("/api/accounts/"+account)
-	.then(function(response) {
-		if(response.ok) {
-			return response.json();
-		}
-		// If account not found, call API to create new account
-		fetch('/api/accounts', {
-			method: 'post',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({'name': account})
+function refreshBalance() {
+	return new Promise(function(resolve, reject){
+		fetch("/api/accounts/"+account)
+		.then(function(response) {
+			if(response.ok) {
+				return response.json();
+			}
+			// If account not found, call API to create new account
+			fetch('/api/accounts', {
+				method: 'post',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({'name': account})
+			})
+			.then(response => response.json())
+			.then(data => {
+				// Display welcome message
+				alert("Welcome to CatanEX, "+data.response.name+"! Your opening balance: $"+data.response.balance);
+				$("#balance").html(data.response.balance);
+				resolve()
+			});
 		})
-		.then(response => response.json())
 		.then(data => {
-			// Display welcome message
-			alert("Welcome to CatanEX, "+data.response.name+"! Your opening balance: $"+data.response.balance);
-			$("#balance").html(data.response.balance);
-		});
-	})
-	.then(data => {
-		$("#balance").html(data.balance)
+			$("#balance").html(data.balance)
+			resolve()
+		})
 	})
 }
 
@@ -126,46 +134,49 @@ const palette = ['#396AB1', '#DA7C30', '#3E9651', '#CC2529', '#535154', '#6B4C9A
 
 // Refresh chart
 function refreshChart() {
-	fetch("/api/trades")
-	.then(function(response) {
-		return response.json();
-	})
-	.then(function(data){
-		var datasets = []
-		$.each(data, function(index){
-			points = []
-			$.each(this.data, function(){
-				points.push({x:new Date(this.timestamp), y:this.price});
-			})
-			points.sort((a, b) => a.x - b.x);
-			datasets.push({label:this.stock, fill:false, borderColor: palette[index], data:points});
+	return new Promise(function(resolve, reject) {
+		fetch("/api/trades")
+		.then(function(response) {
+			return response.json();
 		})
-		var ctx = document.getElementById("stockChart").getContext('2d');
-		var stockChart = new Chart(ctx, {
-				type: 'line',
-				data: {
-						datasets: datasets
-				},
-				options: {
-						elements: {
-							line: {
-									tension: 0, // disables bezier curves
-							}
-						},
-						scales: {
-								xAxes: [{
-										type: 'time',
-										time: {
-											unit: 'minute',
-											displayFormats: {
-												hour: 'hh:mm a'
+		.then(function(data){
+			var datasets = []
+			$.each(data, function(index){
+				points = []
+				$.each(this.data, function(){
+					points.push({x:new Date(this.timestamp), y:this.price});
+				})
+				points.sort((a, b) => a.x - b.x);
+				datasets.push({label:this.stock, fill:false, borderColor: palette[index], data:points});
+			})
+			var ctx = document.getElementById("stockChart").getContext('2d');
+			var stockChart = new Chart(ctx, {
+					type: 'line',
+					data: {
+							datasets: datasets
+					},
+					options: {
+							elements: {
+								line: {
+										tension: 0, // disables bezier curves
+								}
+							},
+							scales: {
+									xAxes: [{
+											type: 'time',
+											time: {
+												unit: 'minute',
+												displayFormats: {
+													hour: 'hh:mm a'
+												},
 											},
-										},
-										position: 'bottom'
-								}]
-						}
-				}
-		});
+											position: 'bottom'
+									}]
+							}
+					}
+			});
+			resolve()
+		})
 	})
 }
 
@@ -192,18 +203,14 @@ $("form").submit(function(event) {
 });
 
 // Connect websocket
-console.log("Connecting via websocket...");
 var stream = new ReconnectingWebSocket('ws://' + location.host + "/stream");
 // called when a message arrives
 stream.onmessage = function(message) {
-	console.log("Received message: " + message.data);
 	alert(message.data);
 };
 stream.onopen = function() {
 	document.getElementById("ws-status").setAttribute("style", "color:black;")
-	console.log("Websocket opened");
 };
 stream.onclose = function(event) {
 	document.getElementById("ws-status").setAttribute("style", "color:lightgray;")
-  console.log("Websocket connection closed: "+JSON.stringify(event));
 };
